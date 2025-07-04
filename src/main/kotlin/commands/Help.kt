51b8d1d2d73d7071768a584f4bb3e7de94665e6c
lheintzmann1 <lheintzmann1@disroot.org>
2025-07-04 20:28:48 +0200

    Moved commands scripts to the commands directory

--- src/main/kotlin/commands/Help.kt
+++ src/main/kotlin/commands/Help.kt
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package kpm.utils
+package kpm.commands
 
 fun help() {
     println("Usage: kpm [command] [options]")
--- src/main/kotlin/commands/Init.kt
+++ src/main/kotlin/commands/Init.kt
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package kpm.utils
+package kpm.commands
 
 import java.util.Scanner
 import java.io.File
