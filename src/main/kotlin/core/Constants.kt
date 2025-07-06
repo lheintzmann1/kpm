547d9297228cb6d424fcca5805dc8a3675170f87
lheintzmann1 <lheintzmann1@disroot.org>
2025-07-06 19:00:26 +0200

    Added some consts

--- src/main/kotlin/core/Constants.kt
+++ src/main/kotlin/core/Constants.kt
@@ -1,7 +1,16 @@
 ﻿package kpm.core
 
+import java.io.File
+
 object Constants {
-    const val KPM_VERSION = $$"${'$'}{project.version}"
+    const val KPM_VERSION: String  = $$"${project.version}"
 
+    val HOME: File = File(System.getProperty("user.home"))
+    val KPM_HOME: File = File("$HOME/.kpm").apply { if (!exists()) mkdirs() }
+    val KPM_TEMPLATES: File = File("$KPM_HOME/templates").apply { if (!exists()) mkdirs() }
 
+    const val TEMPLATES_REPO: String = "https://github.com/KotlinPM/templates.git"
+
+    const val MANIFEST: String = "kpm.json"
+    const val MANIFEST_LOCK: String = "kpm-lock.json"
 }
