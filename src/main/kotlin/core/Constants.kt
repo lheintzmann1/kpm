7e6ef6dec4bbc3b6cee544f344b2d70788eaf25d
lheintzmann1 <lheintzmann1@disroot.org>
2025-07-11 13:09:08 +0200

    Refactored constants file

--- src/main/kotlin/core/Constants.kt
+++ src/main/kotlin/core/Constants.kt
@@ -1,23 +1,31 @@
 ﻿package kpm.core
 
 import java.io.File
+import java.nio.file.Path
+import java.nio.file.Paths
 
 object Constants {
     const val KPM_VERSION: String  = $$"${project.version}"
 
-    val osName = System.getProperty("os.name").lowercase()
+    private val osName = System.getProperty("os.name").lowercase()
     val PLATFORM = when {
-        "windows" in osName -> "windows"
-        listOf("mac", "nix", "sunos", "solaris", "bsd").any { it in osName } -> "*nix"
-        else -> "other"
+        "windows" in osName -> Platform.WINDOWS
+        listOf("mac", "nix", "sunos", "solaris", "bsd").any { it in osName } -> Platform.UNIX
+        else -> Platform.OTHER
     }
-    val HOME: File = File(System.getProperty("user.home"))
-    val KPM_HOME: File = File("$HOME/.kpm").apply { if (!exists()) mkdirs() }
-    val KPM_TEMPLATES: File = File("$KPM_HOME/templates").apply { if (!exists()) mkdirs() }
+    val HOME: Path = Paths.get(System.getProperty("user.home"))
+    val KPM_HOME: Path = HOME.resolve(".kpm")
+    val KPM_TEMPLATES: Path = KPM_HOME.resolve("templates")
 
-//    const val TEMPLATES_REPO: String = "https://github.com/KotlinPM/templates.git"
     const val TEMPLATE_BASE: String = "https://github.com/KotlinPM/template-base"
 
+    const val MANIFEST_SCHEMA: String = "https://raw.githubusercontent.com/KotlinPM/manifest-schema/refs/heads/main/manifest.schema.json"
     const val MANIFEST: String = "kpm.json"
     const val MANIFEST_LOCK: String = "kpm-lock.json"
+}
+
+enum class Platform {
+    WINDOWS,
+    UNIX,
+    OTHER
 }
